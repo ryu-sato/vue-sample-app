838e6bfd921b6a5e08583ecb0490ab4032d586ce
Ryu Sato <ryu@weseek.co.jp>
2019-01-25 00:27:34 +0900

    vue を runtime compile できるよう webpack の alias を設定し、diff2html のテンプレートを compile するサンプルを作成

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const vue =  require('./loaders/vue')
 
+const config = environment.toWebpackConfig()
+
+config.resolve.alias = {
+    'vue$': 'vue/dist/vue.esm.js'
+}
+
 environment.loaders.append('vue', vue)
 module.exports = environment
