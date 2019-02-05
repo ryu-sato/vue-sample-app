a3bd86df35b1e6e7147e6ce623d8ed6a145592e0
Ryu Sato <ryu@weseek.co.jp>
2019-02-06 04:37:10 +0900

    ActiveAdminをインストールし、employeeモデルをCRUD出来るよう実装

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Active Admin is a Ruby on Rails framework for creating elegant backends for website administration. refs: https://github.com/activeadmin/activeadmin/
+gem 'activeadmin'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_05_185733) do
+ActiveRecord::Schema.define(version: 2019_02_05_192323) do
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
 
   create_table "employees", force: :cascade do |t|
     t.string "name", default: "", null: false
