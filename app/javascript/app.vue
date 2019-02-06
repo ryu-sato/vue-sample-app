96ef960b723db07ce4abf90d4e33553f729339a0
Ryu Sato <ryu@weseek.co.jp>
2019-02-07 05:46:03 +0900

    TOPページでemployee モデル一覧を表示できるよう実装

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,43 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <table>
+      <tbody>
+        <tr>
+          <th>name</th>
+          <th>birth</th>
+          <th>department</th>
+          <th>gender</th>
+          <th>joined_date</th>
+          <th>payment</th>
+          <th>note</th>
+        </tr>
+        <tr v-for="e in employees" :key="e.id">
+          <td>{{ e.name }}</td>
+          <td>{{ e.birth }}</td>
+          <td>{{ e.department }}</td>
+          <td>{{ e.gender }}</td>
+          <td>{{ e.joined_date }}</td>
+          <td>{{ e.payment }}</td>
+          <td>{{ e.note }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      employees: []
     }
+  },
+  mounted () {
+    axios
+      .get('/api/v1/employees.json')
+      .then(response => (this.employees = response.data))
   }
 }
 </script>
