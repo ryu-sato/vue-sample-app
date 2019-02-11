272488b504cdfb5db1766a4ec4706545ec94ee59
Ryu Sato <ryu@weseek.co.jp>
2019-02-11 14:53:57 +0900

    モデルのバリデーションを修正

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,9 +3,9 @@ class Employee < ApplicationRecord
 
   enum gender: GENDERS
 
-  validates :gender, inclusion: { in: GENDERS.keys.push(GENDERS.keys.map(&:to_s)) }, exclusion: { in: [nil] }
+  validates :gender, inclusion: { in: GENDERS.keys.concat(GENDERS.keys.map(&:to_s)) }, exclusion: { in: [nil] }
   validates :name, exclusion: { in: [nil, ""] }
   validates :department, exclusion: { in: [nil] }
-  validates :payment, exclusion: { in: [nil] }
+  validates :payment, numericality: true, exclusion: { in: [nil] }
   validates :note, exclusion: { in: [nil] }
 end
