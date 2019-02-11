3f7bef02d094a4a29a6a00d671605dcbc826196d
Ryu Sato <ryu@weseek.co.jp>
2019-02-11 17:58:39 +0900

    モデル作成用Formをコンポーネントとして切り出し

---
+++ app/javascript/EmployeeFormPane.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="$emit('submit')">
+    <div v-if="errors.length != 0">
+      <ul v-for="e in errors" :key="e">
+        <li><font color="red">{{ e }}</font></li>
+      </ul>
+    </div>
+    <div>
+      <label>Name</label>
+      <input v-model="employee.name" type="text">
+    </div>
+    <div>
+      <label>Department</label>
+      <input v-model="employee.department" type="text">
+    </div>
+    <div>
+      <label>Gender</label>
+      <select v-model="employee.gender">
+        <option>other</option>
+        <option>male</option>
+        <option>female</option>
+      </select>
+    </div>
+    <div>
+      <label>Birth</label>
+      <input v-model="employee.birth" type="date">
+    </div>
+    <div>
+      <label>Joined Date</label>
+      <input v-model="employee.joined_date" type="date">
+    </div>
+    <div>
+      <label>Payment</label>
+      <input v-model="employee.payment" type="number" min="0">
+    </div>
+    <div>
+      <label>Note</label>
+      <input v-model="employee.note" type="text">
+    </div>
+    <button type="submit">Commit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    employee: {},
+    errors: ''
+  }
+}
+</script>
+
+<style>
+</style>
--- app/javascript/EmployeeNewPage.vue
+++ app/javascript/EmployeeNewPage.vue
@@ -1,51 +1,17 @@
 <template>
-  <form @submit.prevent="createEmployee">
-    <div v-if="errors.length != 0">
-      <ul v-for="e in errors" :key="e">
-        <li><font color="red">{{ e }}</font></li>
-      </ul>
-    </div>
-    <div>
-      <label>Name</label>
-      <input v-model="employee.name" type="text">
-    </div>
-    <div>
-      <label>Department</label>
-      <input v-model="employee.department" type="text">
-    </div>
-    <div>
-      <label>Gender</label>
-      <select v-model="employee.gender">
-        <option>other</option>
-        <option>male</option>
-        <option>female</option>
-      </select>
-    </div>
-    <div>
-      <label>Birth</label>
-      <input v-model="employee.birth" type="date">
-    </div>
-    <div>
-      <label>Joined Date</label>
-      <input v-model="employee.joined_date" type="date">
-    </div>
-    <div>
-      <label>Payment</label>
-      <input v-model="employee.payment" type="number" min="0">
-    </div>
-    <div>
-      <label>Note</label>
-      <input v-model="employee.note" type="text">
-    </div>
-    <button type="submit">Commit</button>
-  </form>
+  <employee-form-pane :errors="errors" :employee="employee" @submit="createEmployee"></employee-form-pane>
 </template>
 
 <script>
 import axios from 'axios';
 
+import EmployeeFormPane from 'EmployeeFormPane.vue';
+
 export default {
-  data: function () {
+  components: {
+    EmployeeFormPane
+  },
+  data() {
     return {
       employee: {
         name: '',
