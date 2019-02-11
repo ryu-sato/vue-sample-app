49fc1b69f37a1c8a0c8becf48900984ddb631970
Ryu Sato <ryu@weseek.co.jp>
2019-02-11 12:44:21 +0900

    enumに対してもモデルのバリデーションを行う

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,10 @@
 class Employee < ApplicationRecord
-  enum gender: { other: 0, male: 1, female: 2 }
+  GENDERS = { other: 0, male: 1, female: 2 }
 
-  validates :name, exclusion: { in: [nil] }
+  enum gender: GENDERS
+
+  validates :gender, inclusion: { in: GENDERS.keys.push(GENDERS.keys.map(&:to_s)) }, exclusion: { in: [nil] }
+  validates :name, exclusion: { in: [nil, ""] }
   validates :department, exclusion: { in: [nil] }
   validates :payment, exclusion: { in: [nil] }
   validates :note, exclusion: { in: [nil] }
