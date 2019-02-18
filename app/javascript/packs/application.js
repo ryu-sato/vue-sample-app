7ca5bbb0beb641a5f2561ae87451890e86079996
Ryu Sato <ryu@weseek.co.jp>
2019-02-19 04:32:16 +0900

    ActiveAdminのスタイルをアプリケーション全体から除外し、bootstrapをインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,5 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
+import 'bootstrap/dist/js/bootstrap'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+
+// Use vue
 const vue =  require('./loaders/vue')
 
 const config = environment.toWebpackConfig()
@@ -8,4 +10,15 @@ config.resolve.alias = {
 }
 
 environment.loaders.append('vue', vue)
+
+// Use bootstrap (with jquery, popper.js)
+const webpack = require('webpack')
+environment.plugins.append( // Webpacker 3.3.0 以降は set ではなく prepend を使用する
+    'Provide',
+    new webpack.ProvidePlugin({
+        jQuery: 'jquery/dist/jquery',
+        Popper: 'popper.js/dist/popper'
+    })
+)
+
 module.exports = environment
